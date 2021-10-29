842c99a1037a3a7e5a6f522092ad760303a564c3
João Eudes Lima <joaoeudes7@gmail.com>
2021-10-29 16:56:32 -0300

    added support to viewBinding

--- app/src/main/java/klever/hiring/day/android/presentation/MainActivity.kt
+++ app/src/main/java/klever/hiring/day/android/presentation/MainActivity.kt
@@ -3,11 +3,19 @@ package klever.hiring.day.android.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import klever.hiring.day.android.R
+import klever.hiring.day.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
